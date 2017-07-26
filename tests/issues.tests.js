2f41a9e49783063a4b0e6c253865fffae68dbde0
Mark Petrie <petrie.mark@gmail.com>
2017-07-25 21:08:06 -0700

    Fix remaining express migration bugs and pass all tests

--- tests/issues.tests.js
+++ tests/issues.tests.js
@@ -6,67 +6,73 @@ const { assert } = chai;
 const connect = require('../lib/db');
 const url = 'mongodb://localhost:27017/issues-test';
 const app = require('../lib/app');
-const ObjectID = require('mongodb').ObjectID;
 const request = chai.request(app);
 
-describe('issues - POST, GET, PUT', () => {
+const staticIssues = [
+    {
+        // _id: '597529252fb3f719fbebcc29',
+        company: 'ABC Company',
+        contact: 'Samantha Bloom',
+        issue_category: 'User Login',
+        issue_title: 'Account locked due to failed login attempts',
+        issue_sev: 'High',
+        issue_status: 'Open',
+        assigned_to: 'Brad Cooper'
+    },
+    {
+        // _id: '597585d0f9a5f12fda536296',
+        company: 'ABC Company',
+        contact: 'Daren Hackenstack',
+        issue_category: 'Workflow',
+        issue_title: 'Project task does not route to task assignee',
+        issue_sev: 'Medium',
+        issue_status: 'Open',
+        assigned_to: 'Brad Cooper'
+    },
+    {
+        // _id: '597585d0f9a5f12fda536297',
+        company: 'XYZ Company',
+        contact: 'Tara Anderson',
+        issue_category: 'Reports',
+        issue_title: 'Unable to access all of my reports',
+        issue_sev: 'Medium',
+        issue_status: 'Open',
+        assigned_to: 'Shannon Peterson'
+    },
+    {
+        // _id: '597585d0f9a5f12fda536298',
+        company: 'XYZ Company',
+        contact: 'Greg Montgomery',
+        issue_category: 'Notifications',
+        issue_title: 'Email reminders for past due tasks is not working',
+        issue_sev: 'Medium',
+        issue_status: 'Open',
+        assigned_to: 'Shannon Peterson'
+    }
+];
+
+
+describe('issues -', () => {
 
     before(() => connect.connect(url));
     before(() => connect.db.dropDatabase());
 
     it('drops database prior to running tests', () => {
+
         return request.get('/issues')
+
             .then(res => {
                 // console.log('res', res.body.length);
+
                 assert.equal(res.body.length, 0);
             });
     });
+});
 
-    const issues = [
-        {
-            // _id: '597529252fb3f719fbebcc29',
-            company: 'ABC Company',
-            contact: 'Samantha Bloom',
-            issue_category: 'User Login',
-            issue_title: 'Account locked due to failed login attempts',
-            issue_sev: 'High',
-            issue_status: 'Open',
-            assigned_to: 'Brad Cooper'
-        },
-        {
-            // _id: '597585d0f9a5f12fda536296',
-            company: 'ABC Company',
-            contact: 'Daren Hackenstack',
-            issue_category: 'Workflow',
-            issue_title: 'Project task does not route to task assignee',
-            issue_sev: 'Medium',
-            issue_status: 'Open',
-            assigned_to: 'Brad Cooper'
-        },
-        {
-            // _id: '597585d0f9a5f12fda536297',
-            company: 'XYZ Company',
-            contact: 'Tara Anderson',
-            issue_category: 'Reports',
-            issue_title: 'Unable to access all of my reports',
-            issue_sev: 'Medium',
-            issue_status: 'Open',
-            assigned_to: 'Shannon Peterson'
-        },
-        {
-            // _id: '597585d0f9a5f12fda536298',
-            company: 'XYZ Company',
-            contact: 'Greg Montgomery',
-            issue_category: 'Notifications',
-            issue_title: 'Email reminders for past due tasks is not working',
-            issue_sev: 'Medium',
-            issue_status: 'Open',
-            assigned_to: 'Shannon Peterson'
-        }
-    ];
-
+describe('issues -', () => {
     it('saves new issues', () => {
-        return Promise.all(issues.map((issue, i) => {
+
+        return Promise.all(staticIssues.map((issue, i) => {
             return request.post('/issues')
                 .send(issue)
                 .then(res => {
@@ -74,40 +80,37 @@ describe('issues - POST, GET, PUT', () => {
                     assert.ok(saved._id);
                     assert.equal(saved.company, issue.company);
                     assert.equal(saved.contact, issue.contact);
-                    issues[i] = saved;
+                    staticIssues[i] = saved;
                 });
         }));
     });
+});
 
+describe('issues -', () => {
     it('returns all issues', () => {
         return request.get('/issues')
             .then(res => {
-                // console.log(res.body);
                 let saved = res.body;
-                // console.log(res.body);
                 assert.equal(saved.length, 4);
-                assert.equal(saved[0].contact, issues[2].contact);
-                assert.equal(saved[0].issue_title, issues[2].issue_title);
+                assert.equal(saved[0].contact, staticIssues[0].contact);
+                assert.equal(saved[0].issue_title, staticIssues[0].issue_title);
             });
     });
+});
 
+describe('issues -', () => {
     it('returns requested issue', () => {
-        return request.get(`/issues/${issues[0]._id}`)
-            // .send({ _id: '597529252fb3f719fbebcc29' })
+        return request.get(`/issues/${staticIssues[0]._id}`)
             .then(res => {
                 let results = res.body;
-                console.log(results);
-                // console.log(results);
-                assert.equal(results.contact, issues[0].contact);
+                assert.equal(results.contact, staticIssues[0].contact);
             });
     });
+});
 
-
-
+describe('issues -', () => {
     it('updates desired issue', () => {
-        // let putId = { _id: new ObjectID(issues[0]._id) };
-        
-        return request.put(`/issues/${ issues[0]._id }`)
+        return request.put(`/issues/${staticIssues[0]._id}`)
             .send({
                 company: 'ABC Company',
                 contact: 'Samantha Bloom',
@@ -119,36 +122,39 @@ describe('issues - POST, GET, PUT', () => {
             })
             .then(res => {
                 let results = res.body;
-                console.log('res.body: ', res.body);
-                assert.equal(issues[0].issue_status, 'Closed');
+                assert.equal(results.issue_status, 'Closed');
             });
     });
+});
 
-    describe('issues - DEL', () => {
+describe('issues -', () => {
 
-        it('deletes requested issue, responds with { removed: true } message', () => {
-            return request.del('/issues/597529252fb3f719fbebcc29')
-                .then(res => {
-                    let results = res.body;
-                    assert.deepEqual(results, { removed: true });
-                });
-        });
+    it('deletes requested issue, responds with { removed: true } message', () => {
+        return request.del(`/issues/${staticIssues[0]._id}`)
+            .then(res => {
+                let results = res.body;
+                assert.deepEqual(results, { removed: true });
+            });
+    });
+});
 
-        it('returns 404 not found if requested document does not exist', () => {
-            return request.get('/issues/597529252bb3f719fbebcc31')
-                .then(
-                    () => { throw new Error('Expected 404 error instead got 200'); },
-                    err => assert.ok(err.response.notFound)
-                );
+describe('issues -', () => {
 
-        });
+    it('returns 404 not found if requested document does not exist', () => {
+        return request.get('/issues/597813f5f107c3d099b69a19')
+            .then((res) => 
+                () => { throw new Error('Expected 404 error instead got 200'); },
+            err => assert.ok(err.response.notFound)
+            );
+    });
+});
 
-        it('returns { removed: false } message for requests to delete non-existent issues', () => {
-            return request.del('/issues/597529252bb3f719fbebcc31')
-                .then(res => {
-                    let results = res.body;
-                    assert.deepEqual(results, { removed: false });
-                });
-        });
+describe('issues -', () => {
+    it('returns { removed: false } message for requests to delete non-existent issues', () => {
+        return request.del(`/issues/${staticIssues[0]._id}`)
+            .then(res => {
+                let results = res.body;
+                assert.deepEqual(results, { removed: false });
+            });
     });
 });
